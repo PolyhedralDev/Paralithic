cdfeb593dd3dca3d274b5702e7c337415b88f6df
dfsek <dfsek@protonmail.com>
2021-02-03 23:02:29 -0700

    sourcesJar task

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,11 @@ dependencies {
     testImplementation("junit", "junit", "4.12")
 }
 
+val sourcesJar by tasks.registering(Jar::class) {
+    classifier = "sources"
+    from(sourceSets.main.get().allSource)
+}
+
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
