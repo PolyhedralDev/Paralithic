b669bdf48d79e4eab50ccdae7928d00cdd0d8413
dfsek <dfsek@protonmail.com>
2021-02-03 22:58:16 -0700

    Add build stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
+import java.io.ByteArrayOutputStream
+
 plugins {
     java
+    `maven-publish`
 }
 
+val versionObj = Version("0", "1", "0", true)
+
+
 group = "com.dfsek"
-version = "1.0-SNAPSHOT"
+version = versionObj
 
 repositories {
     mavenCentral()
@@ -20,4 +26,51 @@ dependencies {
     testImplementation("com.scireum:parsii:1.2.1")
     testImplementation("net.objecthunter:exp4j:0.4.8")
     testImplementation("junit", "junit", "4.12")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["jar"])
+        }
+    }
+
+    repositories {
+        val mavenUrl = "https://repo.codemc.io/repository/maven-releases/"
+        val mavenSnapshotUrl = "https://repo.codemc.io/repository/maven-snapshots/"
+
+        maven((if (versionObj.preRelease) mavenSnapshotUrl else mavenUrl)) {
+            val mavenUsername: String? by project
+            val mavenPassword: String? by project
+            if (mavenUsername != null && mavenPassword != null) {
+                credentials {
+                    username = mavenUsername
+                    password = mavenPassword
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Version class that does version stuff.
+ */
+@Suppress("MemberVisibilityCanBePrivate")
+class Version(val major: String, val minor: String, val revision: String, val preRelease: Boolean = false) {
+
+    override fun toString(): String {
+        return if (!preRelease)
+            "$major.$minor.$revision"
+        else //Only use git hash if it's a prerelease.
+            "$major.$minor.$revision+${getGitHash()}"
+    }
+}
+fun getGitHash(): String {
+    val stdout = ByteArrayOutputStream()
+    exec {
+        commandLine = mutableListOf("git", "rev-parse", "--short", "HEAD")
+        standardOutput = stdout
+    }
+    return stdout.toString().trim()
 }
