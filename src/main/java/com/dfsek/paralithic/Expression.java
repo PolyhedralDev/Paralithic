01676aaafb291148d2139dca5f68ac5edf662fca
dfsek <dfsek@protonmail.com>
2021-07-19 11:39:40 -0700

    implement context in Expression

--- src/main/java/com/dfsek/paralithic/Expression.java
+++ src/main/java/com/dfsek/paralithic/Expression.java
@@ -1,5 +1,12 @@
 package com.dfsek.paralithic;
 
+import com.dfsek.paralithic.functions.dynamic.Context;
+
 public interface Expression {
-    double evaluate(double... args);
+    Context DEFAULT_CONTEXT = new Context() {};
+    default double evaluate(double... args) {
+        return evaluate(DEFAULT_CONTEXT, args);
+    }
+
+    double evaluate(Context context, double... args);
 }
