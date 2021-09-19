14b0e7cc71c5b6382e3409e91d36a9f2e07701c5
dfsek <dfsek@protonmail.com>
2021-09-18 20:42:32 -0700

    DCONST_0 and DCONST_1 simplifications

--- src/main/java/com/dfsek/paralithic/operations/Constant.java
+++ src/main/java/com/dfsek/paralithic/operations/Constant.java
@@ -2,9 +2,34 @@ package com.dfsek.paralithic.operations;
 
 import org.jetbrains.annotations.NotNull;
 import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 
-public class Constant implements Node {
+public class Constant implements Simplifiable {
     protected final double value;
+
+    public static final Constant DCONST_0 = new Constant(0) {
+        @Override
+        public void apply(@NotNull MethodVisitor visitor, String generatedImplementationName) {
+            visitor.visitInsn(Opcodes.DCONST_0);
+        }
+
+        @Override
+        public @NotNull Node simplify() {
+            return this;
+        }
+    };
+
+    public static final Constant DCONST_1 = new Constant(1) {
+        @Override
+        public void apply(@NotNull MethodVisitor visitor, String generatedImplementationName) {
+            visitor.visitInsn(Opcodes.DCONST_1);
+        }
+
+        @Override
+        public @NotNull Node simplify() {
+            return this;
+        }
+    };
 
     private Constant(double value) {
         this.value = value;
@@ -25,5 +50,12 @@ public class Constant implements Node {
 
     public void apply(@NotNull MethodVisitor visitor, String generatedImplementationName) {
         visitor.visitLdcInsn(value);
+    }
+
+    @Override
+    public @NotNull Node simplify() {
+        if(value == 0) return DCONST_0;
+        if(value == 1) return DCONST_1;
+        return this;
     }
 }
