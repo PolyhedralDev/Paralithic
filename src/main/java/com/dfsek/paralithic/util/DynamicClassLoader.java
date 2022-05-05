0d9776b9e170a63e1cddcfe3c73037b412ff5b7e
dfsek <dfsek@protonmail.com>
2022-05-05 13:19:01 -0700

    specify parent loader of DynamicClassLoader

--- src/main/java/com/dfsek/paralithic/util/DynamicClassLoader.java
+++ src/main/java/com/dfsek/paralithic/util/DynamicClassLoader.java
@@ -1,6 +1,12 @@
 package com.dfsek.paralithic.util;
 
+import com.dfsek.paralithic.Expression;
+
 public class DynamicClassLoader extends ClassLoader {
+    public DynamicClassLoader() {
+        super(Expression.class.getClassLoader());
+    }
+
     public Class<?> defineClass(String name, byte[] data) {
         return defineClass(name, data, 0, data.length);
     }
