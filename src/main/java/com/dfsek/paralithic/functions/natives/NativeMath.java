c9391105daf7e7b62c4a422eeb708f7b45e18bdb
dfsek <dfsek@dfsek.com>
2024-04-10 07:53:03 -0700

    add pow optimisation cases

--- src/main/java/com/dfsek/paralithic/functions/natives/NativeMath.java
+++ src/main/java/com/dfsek/paralithic/functions/natives/NativeMath.java
@@ -1,8 +1,15 @@
 package com.dfsek.paralithic.functions.natives;
 
+import com.dfsek.paralithic.node.Constant;
+import com.dfsek.paralithic.node.binary.number.DivisionNode;
+import com.dfsek.paralithic.node.special.function.NativeFunctionNode;
+
+import java.util.List;
+import java.util.Optional;
+
 public class NativeMath {
     private static final Class<?> MATH = Math.class;
-    public static NativeFunction POW = (NativeMathFunction) () -> MATH.getMethod("pow", double.class, double.class);
+
     public static NativeFunction MAX = (NativeMathFunction) () -> MATH.getMethod("max", double.class, double.class);
     public static NativeFunction MIN = (NativeMathFunction) () -> MATH.getMethod("min", double.class, double.class);
     public static NativeFunction SIN = (NativeMathFunction) () -> MATH.getMethod("sin", double.class);
@@ -12,6 +19,25 @@ public class NativeMath {
     public static NativeFunction FLOOR = (NativeMathFunction) () -> MATH.getMethod("floor", double.class);
     public static NativeFunction CEIL = (NativeMathFunction) () -> MATH.getMethod("ceil", double.class);
     public static NativeFunction SQRT = (NativeMathFunction) () -> MATH.getMethod("sqrt", double.class);
+    public static NativeFunction POW = ((NativeMathFunction) () -> MATH.getMethod("pow", double.class, double.class)).withSimplifyRule(args -> {
+        if(args.get(1) instanceof Constant c) { // constant powers
+            double v = c.getValue();
+            if(v == 0) {
+                return Optional.of(Constant.of(1)); // n^0 == 1
+            } else if(v == 1) {
+                return Optional.of(args.get(0)); // n^1 == n
+            } else if(v == -1) {
+                return Optional.of(new DivisionNode(Constant.of(1), args.get(0))); // n^-1 = 1/n
+            } else if(v == 0.5) {
+                return Optional.of(new NativeFunctionNode(SQRT, List.of(args.get(0)))); // n^0.5 == sqrt(n)
+            }
+        }
+        if(args.get(0) instanceof Constant c) {
+            double v = c.getValue();
+            if(v == 1) return Optional.of(Constant.of(1)); // 1^n == 1
+        }
+        return Optional.empty();
+    });
     public static NativeFunction SINH = (NativeMathFunction) () -> MATH.getMethod("sinh", double.class);
     public static NativeFunction COSH = (NativeMathFunction) () -> MATH.getMethod("cosh", double.class);
     public static NativeFunction TANH = (NativeMathFunction) () -> MATH.getMethod("tanh", double.class);
