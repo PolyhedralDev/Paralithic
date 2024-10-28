f3dc1f63e21243a1d457353df85633f9030e03b5
Astrash <astrash@atr.sh>
2024-10-28 13:20:52 +1100

    Implement simplification and statefulness for LV binding node

--- src/main/java/com/dfsek/paralithic/node/special/LocalVariableBindingNode.java
+++ src/main/java/com/dfsek/paralithic/node/special/LocalVariableBindingNode.java
@@ -1,17 +1,18 @@
 package com.dfsek.paralithic.node.special;
 
-import com.dfsek.paralithic.node.Node;
-import com.dfsek.paralithic.node.NodeUtils;
-import com.dfsek.paralithic.node.Statefulness;
+import com.dfsek.paralithic.node.*;
+import com.dfsek.paralithic.util.Lazy;
 import org.jetbrains.annotations.NotNull;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
-public class LocalVariableBindingNode implements Node {
+public class LocalVariableBindingNode implements Simplifiable {
 
     private final int index;
-    private final Node boundExpression;
-    private final Node expression;
+    private Node boundExpression;
+    private Node expression;
+
+    private final Lazy<Statefulness> statefulness = Lazy.of(() -> Statefulness.combine(boundExpression.statefulness(), expression.statefulness()));
 
     public LocalVariableBindingNode(int index, Node boundExpression, Node expression) {
         this.index = index;
@@ -28,12 +29,30 @@ public class LocalVariableBindingNode implements Node {
 
     @Override
     public Statefulness statefulness() {
-        return null;
+        return statefulness.get();
     }
 
     @Override
     public double eval(double[] localVariables, double... inputs) {
         localVariables[index] = boundExpression.eval(localVariables, inputs);
         return expression.eval(localVariables, inputs);
+    }
+
+    @Override
+    public @NotNull Node simplify() {
+        boundExpression = NodeUtils.simplify(boundExpression);
+
+        // A dead code optimization could be made here if the local variable is never
+        // referenced inside `this.expression` however such an optimization would add
+        // extra complexity (possibly via some kind of ref-counting mechanism) for
+        // something that JIT will likely handle well anyway.
+        expression = NodeUtils.simplify(expression);
+
+        statefulness.invalidate();
+
+        if (expression instanceof Constant)
+            return expression;
+
+        return this;
     }
 }
